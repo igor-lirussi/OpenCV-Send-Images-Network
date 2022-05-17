2cf99299776a6d3a3d7cddc3a6a304bc638190fa
unknown <igor.lirussi@studio.unibo.it>
2022-05-17 13:06:30 +0300

    added reading raw stream of data as image

---
+++ OpenCV_read_raw_stream.py
@@ -0,0 +1,35 @@
+import cv2
+import urllib.request
+import numpy as np
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--s', help='Source to play, es: --s=http://79.123.176.58:8080', default="http://79.123.176.58:8080")
+args = parser.parse_args()
+
+print("--"*30)
+print("passed:")
+print(args.s)
+print("--"*30)
+
+
+#if len(sys.argv)>1:
+#    host = sys.argv[1]
+
+hoststr = args.s
+
+stream=urllib.request.urlopen(hoststr)
+
+bytes=b''
+while True:
+    bytes+=stream.read(1024)
+    a = bytes.find(b'\xff\xd8')
+    b = bytes.find(b'\xff\xd9')
+    if a!=-1 and b!=-1:
+        jpg = bytes[a:b+2]
+        bytes= bytes[b+2:]
+        i = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8),cv2.IMREAD_COLOR)
+        cv2.imshow(hoststr,i)
+        if cv2.waitKey(1) ==27:
+            exit(0)
